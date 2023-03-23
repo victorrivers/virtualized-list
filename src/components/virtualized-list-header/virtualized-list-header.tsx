841bb59e198696ea8ee0cd0a51cddb1f1877170c
Victor <victor_github@outlook.com>
2023-03-23 13:43:03 +0100

    Header alignment. Styles cleanup.

--- src/components/virtualized-list-header/virtualized-list-header.tsx
+++ src/components/virtualized-list-header/virtualized-list-header.tsx
@@ -2,24 +2,26 @@ import React from "react";
 import styles from "./virtualized-list-header.module.css";
 
 type VirtualizedListHeaderProps = {
-	columns: string[];
-	className?: string;
+  columns: string[];
+  className?: string;
 };
 
 export default function VirtualizedListHeader({
-	columns,
-	className = "",
+  columns,
+  className = "",
 }: VirtualizedListHeaderProps): JSX.Element {
-	const [id, name, material, price] = columns;
-	return (
-		<div className={`${styles.header} ${className}`}>
-			<div className={styles.col}>{capitalize(name)}</div>
-			<div className={styles.col}>{capitalize(price)}</div>
-			<div className={styles.col}>{capitalize(material)}</div>
-		</div>
-	);
+  const [id, name, material, price] = columns;
+  return (
+    <div className={styles.scrollOffset} style={{ overflowY: "scroll" }}>
+      <div className={`${styles.header} ${className}`}>
+        <div className={styles.col}>{capitalize(name)}</div>
+        <div className={styles.col}>{capitalize(price)}</div>
+        <div className={styles.col}>{capitalize(material)}</div>
+      </div>
+    </div>
+  );
 }
 
 function capitalize(value: string) {
-	return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
+  return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
 }
